215cff13364e32bf15e14b32909969bf19e6f568
SIVASURYA <112889318+sivasurya01@users.noreply.github.com>
2024-05-10 21:29:04 -0700

    first

--- src/learn.js
+++ src/learn.js
@@ -19,3 +19,31 @@ function* generator(i) {
 const gen = generator(10);
 
 console.log(gen.next().value);
+do {
+  console.log("yes");
+} while (array.length == 10);
+let day = 31;
+for (let index = 0; index <= day; index++) {
+  const element = index;
+  console.log(element);
+}
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(2, 2));
+function factorial(num) {
+  if (num > 1) {
+    return num * factorial(num - 1);
+  } else {
+    return 1;
+  }
+}
+
+console.log(factorial(5));
+let month = {
+  1: "janury",
+  2: "febrary",
+  3: "march",
+  4: "aprll",
+};
+console.log(month[3]);
